e1ea706532c1cc7ea30fa71748d047d72e036f41
salman7937 <salmanahmed7937@gmail.com>
2024-12-18 10:22:43 +0500

    change the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import image  from "../assets/logo2.png"
 function Footer() {
     return (
-        <div className='bg-black text-white md:mt-[60px] mt-10 py-6 px-20 flex justify-between items-center'>
+        <div className='bg-black text-white md:mt-[60px] mt-10 py-6 md:px-20 px-4 flex md:flex-row  flex-col justify-between items-center'>
             <div className='flex items-center  gap-3' >
                 <img src={image} alt="" />
-                <h5 className='text-xl font-bold tracking-[-2%] '>Salman Ahmad</h5>
+                <h5 className=' md:text-xl text-lg font-bold tracking-[-2%] '>Salman Ahmad</h5>
 
             </div>
-            <div className=' flex flex-col items-end '>
+            <div className=' flex flex-col items-end md:text-base text-sm '>
                 <p>copyright © 2025 All rights reserverd. </p>
                 
                 
